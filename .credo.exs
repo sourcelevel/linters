b6664b11123a9dbce88d1286e814756f12ff3e68
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2020-07-28 10:43:35 -0300

    Remove TODO mention in .credo.exs
    Co-authored-by: garaujodev <garaujodev@users.noreply.github.com>

--- .credo.exs
+++ .credo.exs
@@ -81,7 +81,7 @@
         #
         {Credo.Check.Design.DuplicatedCode, excluded_macros: []},
         # You can also customize the exit_status of each check.
-        # If you don't want TODO comments to cause `mix credo` to fail, just
+        # If you don't want these comments to cause `mix credo` to fail, just
         # set this value to 0 (zero).
         #
         # Disabled for now as they are also checked by Code Climate
